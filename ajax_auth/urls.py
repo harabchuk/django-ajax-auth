3ee0a034779907f4e34abc26b8c2a7d75fca3bf9
FrancoAA <franco.alarcon@gmail.com>
2014-07-02 17:36:14 -0300

    added name the urls

--- ajax_auth/urls.py
+++ ajax_auth/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import *
 from ajax_auth import views
 
 urlpatterns = patterns('',
-                       url(r'^login/$', views.LoginView.as_view()),
-                       url(r'^logout/$', views.LogoutView.as_view()),
-                       url(r'^register/$', views.RegisterView.as_view()),
+                       url(r'^login/$', views.LoginView.as_view(), name='ajax-auth-login'),
+                       url(r'^logout/$', views.LogoutView.as_view(), name='ajax-auth-logout'),
+                       url(r'^register/$', views.RegisterView.as_view(), name='ajax-auth-register'),
 )
